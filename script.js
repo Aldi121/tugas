80c86ecd7456ed6846dc80f3c76cdf179b065e3d
wraxelan-hash <wraxelan@gmail.com>
2025-11-20 21:45:56 +0800

    tolong saya

--- script.js
+++ script.js
@@ -17,16 +17,9 @@ function tampilkanGambar(src) {
     }, 50);
 }
 
-tombol.addEventListener("click", () => {
-    setTimeout(() => {
-        gambar.classList.add("muncul");
-        teks.classList.add("muncul");
-    }, 30);
-});
-
 
 btn1.addEventListener("click", function () {
-    tampilkanGambar("wizzy.jpg");
+    tampilkanGambar("wisnu militer.jpg");
 });
 
 // Tombol 2
@@ -37,17 +30,4 @@ btn2.addEventListener("click", function () {
 // Tombol 3
 btn3.addEventListener("click", function () {
     tampilkanGambar("gambar3.jpg");
-});
-
-const tombol = document.getElementById("tombol");
-const semuaGambar = document.querySelectorAll(".gambar-item");
-
-tombol.addEventListener("click", () => {
-    setTimeout(() => {
-        semuaGambar.forEach((item, index) => {
-            setTimeout(() => {
-                item.classList.add("muncul");
-            }, index * 120); // animasi muncul satu per satu
-        });
-    }, 30);
 });
