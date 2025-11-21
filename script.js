58f80205e3abbd384c838661ffaf7fa25bbb92d2
wraxelan-hash <wraxelan@gmail.com>
2025-11-21 20:55:11 +0800

    baikin bugnya dong

--- script.js
+++ script.js
@@ -1,40 +1,24 @@
-
 const btn1 = document.getElementById("satu");
 const btn2 = document.getElementById("dua");
 const btn3 = document.getElementById("tiga");
 
-
 const gambar = document.querySelector(".gambar img");
+const video = document.querySelector(".video video");
 
-function tampilkanGambar(src) {
-    gambar.classList.remove("muncul"); 
-
-    // Tunggu sedikit agar efek fade bisa restart
+function ubahMedia(imgSrc, videoSrc) {
+    // ubah gambar
+    gambar.classList.remove("muncul");
+    video.classList.remove("muncul");
     setTimeout(() => {
-        gambar.src = src;
-        gambar.style.display = "block"
+        gambar.src = imgSrc;
         gambar.classList.add("muncul");
-    }, 50);
+        video.src = videoSrc;
+        video.load();
+        video.pause();
+        video.classList.add("muncul");
+    }, 100);
 }
 
-tombol.addEventListener("click", () => {
-    setTimeout(() => {
-        gambar.classList.add("muncul");
-        teks.classList.add("muncul");
-    }, 30);
-});
-
-
-btn1.addEventListener("click", function () {
-    tampilkanGambar("wizzy.jpg");
-});
-
-// Tombol 2
-btn2.addEventListener("click", function () {
-    tampilkanGambar("Random Stream.jpg");
-});
-
-// Tombol 3
-btn3.addEventListener("click", function () {
-    tampilkanGambar("gambar3.jpg");
-});
+btn1.addEventListener("click", () => ubahMedia("wizzy.jpg", "gas.mp4"));
+btn2.addEventListener("click", () => ubahMedia("Random Stream.jpg", "lele.mp4"));
+btn3.addEventListener("click", () => ubahMedia("gambar3.jpg", "alpukat.mp4"));
